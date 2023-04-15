fdfadcce86347749ea00d15d5d7dbe1a58143a4f
Nate Wilcox <nathan+dev@electriccoin.co>
2023-04-15 16:10:41 -0700

    Initial cli structure.

---
+++ src/cli.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+
+/// A full-terminal interactive shell
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Options {}
+
+pub fn run() -> anyhow::Result<()> {
+    let _ = Options::parse();
+    todo!()
+}
